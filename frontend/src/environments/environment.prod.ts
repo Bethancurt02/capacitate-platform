74d706931f17309d988ca17d1f5c6237433004b6
Bethancurt02 <bethancurtvaldeze@gmail.com>
2026-05-05 10:31:28 -0400

    Add files via upload

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Esta URL se usará cuando compiles para producción. 
   // Debes reemplazarla por la URL real de tu backend cuando lo subas a Render.
-  apiUrl: 'https://capacitate-backend.onrender.com/api'
+  apiUrl: 'https://capacitate-platform.vercel.app/api'
 };
